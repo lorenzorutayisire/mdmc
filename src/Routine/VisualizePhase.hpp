00c187d8cb0c01878ee671287ff25fe743c3420c
loryruta <loryruta23@gmail.com>
2019-10-03 19:34:05 +0200

    Remove old Phases

--- src/Routine/VisualizePhase.hpp
+++ src/Routine/VisualizePhase.hpp
@@ -59,7 +59,7 @@ private:
 public:
 	VisualizePhase(Scene* scene)
 	{
-		this->voxelizer = std::make_shared<Voxelizer>(Voxelizer(*scene, 64));
+		this->voxelizer = std::make_shared<Voxelizer>(Voxelizer(*scene, 256));
 		this->camera_mode = 'c';
 	}
 
